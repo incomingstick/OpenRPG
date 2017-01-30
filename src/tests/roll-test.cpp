73a61dedcec22c761fdd8b1f6d282f1cb4f8dda8
incomingstick <incomingstick@gmail.com>
2017-01-30 16:21:07 -0600

    introduction of roll test suite

---
+++ src/tests/roll-test.cpp
@@ -0,0 +1,17 @@
+/*
+roll.cpp - Test program for roll module
+Created on: Dec 1, 2016
+
+License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
+This is free software: you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    cout << "Testing Roll module now" << endl;
+
+    return 0;
+}
