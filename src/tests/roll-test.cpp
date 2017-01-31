150187174ce83aef60357507baf0d2bac7d0f190
incomingstick <incomingstick@gmail.com>
2017-01-30 21:10:02 -0600

    roll module test suite added

--- src/tests/roll-test.cpp
+++ src/tests/roll-test.cpp
@@ -7,11 +7,31 @@ This is free software: you are free to change and redistribute it.
 There is NO WARRANTY, to the extent permitted by law.
 */
 #include <iostream>
+#include <string>
+#include <map>
+#include <random>
+
+#include "die.h"
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
-    cout << "Testing Roll module now" << endl;
+/* taken from http://en.cppreference.com/w/cpp/numeric/random/random_device
+    to test random_device output */
+int main() {
+    std::random_device rd;
+    std::map<int, int> hist;
 
-    return 0;
+    Die d6(6);
+
+    for (int n = 0; n < 20000; ++n) {
+        ++hist[d6.roll()]; // note: demo only: the performance of many 
+                          // implementations of random_device degrades sharply
+                          // once the entropy pool is exhausted. For practical use
+                          // random_device is generally only used to seed 
+                          // a PRNG such as mt19937
+    }
+    
+    for (auto p : hist) {
+        std::cout << p.first << " : " << std::string(p.second/100, '*') << '\n';
+    }
 }
