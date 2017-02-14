82b18e3c7bdd2ea912b2e1c2111e338ea06f21b4
incomingstick <incomingstick@gmail.com>
2017-02-13 19:17:46 -0600

    added algorithm rules to test

--- src/lib/modules/roll/roll-parser.cpp
+++ src/lib/modules/roll/roll-parser.cpp
@@ -393,6 +393,23 @@ void ExpressionTree::parse_expression(void) {
     int numBytesRead = 0;
     int numBytesToRead = 0;
 
+    struct parse_node* cur = head;
+
+    /* Basic rules of a math expression parser
+    1) If the current token is a '(', add a new node as the left
+        child of the current node, and descend to the left child.
+
+    2) If the current token is in the list ['+','-','/','*'],
+        set the root value of the current node to the operator
+        represented by the current token. Add a new node as the
+        right child of the current node and descend to the right child.
+
+    3) If the current token is a number, set the root value of the
+        current node to the number and return to the parent.
+
+    4) If the current token is a ')', go to the parent of the current node.
+    */
+
     /* TODO we should scan the string to create the tree and parse out the expression */
     for(string::iterator it = inputString.begin(); it != inputString.end(); ++it) {
         char cur_ch = *it;
