fa8b3fdf99620f2eee94936a5197d7d32a9754f2
incomingstick <incomingstick@gmail.com>
2019-04-22 16:13:13 -0500

    added install and uninstall functionality to build.sh

--- build.sh
+++ build.sh
@@ -2,28 +2,21 @@
 
 cwd=$(pwd)
 
-#TODO print a help output for this script.
+# TODO print a help output for this script.
 # This should not have to adhear to our help output standards
 # as the only people really ever running this script are devs
 if [[ $1 == "help" || $1 == "h" ]]; then
     echo "This option is not yet supported: $1"
-    exit 0
+    exit 1
 fi
 
+# TODO finish this logics
 if [[ $1 == "clean" || $1 == "rebuild" && -d "$cwd/build" ]]; then
     rm -rfv "$cwd/build"
 
     if [[ $1 == "clean" ]]; then
         exit 0
     fi
-fi
-
-#TODO add install command line option
-# Should we rebuild and then install, or install
-# using the last build on the system?
-if [[ $1 == "install" || $2 == "install" ]]; then
-    echo "This option is not yet supported: $2"
-    exit 0
 fi
 
 if [[ $OSTYPE == "linux"* || $OSTYPE == "darwin"*  ||  $OSTYPE == "cygwin" ]]; then
@@ -35,10 +28,28 @@ if [[ $OSTYPE == "linux"* || $OSTYPE == "darwin"*  ||  $OSTYPE == "cygwin" ]]; t
 
             pushd $cwd/build/
 
-            #TODO we should check the arch we are building for?
+            # Should we rebuild and then install, or install
+            # using the last build on the system?
+            if [[ $1 == "uninstall" || $2 == "uninstall" ]]; then
+                sudo make uninstall
+                popd
+                
+                exit 0
+            fi
+
+            # TODO we should check the arch we are building for?
             
             if [[ ! -a "Makefile" ]]; then
                 cmake ..
+            fi
+            
+            # Should we rebuild and then install, or install
+            # using the last build on the system?
+            if [[ $1 == "install" || $2 == "install" ]]; then
+                sudo make install
+                popd
+
+                exit 0
             fi
 
             make
