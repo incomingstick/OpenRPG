7e98102601f7142fd4a10b1083cbff0286285f64
incomingstick <incomingstick@gmail.com>
2019-04-24 16:38:26 -0500

    build script build.sh fixed and imrpoved some logic

--- build.sh
+++ build.sh
@@ -12,7 +12,8 @@ if [[ $1 == "help" || $1 == "h" ]]; then
     exit 1
 fi
 
-# TODO finish this logics
+# TODO improve all of the argument parsing to be way more efficient
+
 if [[ $1 == "clean" || $1 == "rebuild" && -d "$cwd/build" ]]; then
     rm -rfv "$cwd/build"
 
@@ -60,8 +61,6 @@ if [[ $OSTYPE == "linux"* || $OSTYPE == "darwin"*  ||  $OSTYPE == "cygwin" ]]; t
 
             make
             popd
-
-            exit 0
         else
             echo "MAKE NOT INSTALLED, ABORTING..."
             exit 1
@@ -70,46 +69,50 @@ if [[ $OSTYPE == "linux"* || $OSTYPE == "darwin"*  ||  $OSTYPE == "cygwin" ]]; t
         echo "CMAKE NOT INSTALLED, ABORTING..."
         exit 1
     fi
-}
-
-built_api(){
-   if hash cargo 2>/dev/null; then
-        if [[ ! -a "Cargo.toml" ]]; then
-
-              pushd $cwd/api/
-
-              cargo build
-
-              popd
-        else
-              echo "CARGO.TOML NOT FOUND..."
-        fi
-   else
-        echo "CARGO/RUST NOT INSTALLED..."
-   fi
-}
-
-if [[ $OSTYPE == "linux"* || $OSTYPE == "darwin"*  ||  $OSTYPE == "cygwin" ]] && [[ $# -eq 0 ]]; then
-    build_cpp
-elif [[ "$1" == "api" ]]; then
-    echo "BUILDING CPP SOURCE..."
-    build_cpp
-    echo "FINISHED.... MOVING FILES..."
-    mkdir $cwd/api/build/
-    cp $cwd/build/{character-generator,name-generator,roll,openrpg} $cwd/api/build/ 
-    echo "FINISHED.... BUILDING API..."
-    built_api
-elif [[ "$1" == "clean-api" ]]; then
-    if [ -d "$cwd/api/build/" ]; then
-      echo "REMOVING API/BUILD"  
-      rm -r "$cwd/api/build/"
-    fi
-    pushd $cwd/api/
-    echo "CLEANING API DIR"
-    cargo clean
-    echo "API CLEAN... FINISHED"
-    popd
 else
     echo "UNSUPORTED PLATFORM $OSTYPE"
     exit 1
+fi
+
+if [[ $1 == "api" || $2 == "api" ]]; then
+    if hash cargo 2>/dev/null; then
+        if [[ ! -a "Cargo.toml" ]]; then
+            if [[ $1 == "rebuild" || $2 == "rebuild" || $1 == "clean" || $2 == "clean" ]]; then
+                if [ -d "$cwd/api/build/" ]; then
+                    echo "REMOVING API/BUILD"  
+                    rm -r "$cwd/api/build/"
+                fi
+
+                pushd $cwd/api/
+                echo "CLEANING API DIR"
+
+                cargo clean
+
+                echo "API CLEAN... FINISHED"
+                popd
+
+                if [[ $1 == "clean" || $2 == "clean" ]]; then
+                    exit 0
+                fi
+            fi
+
+            echo "MOVING FILES FOR CARGO..."
+
+            mkdir $cwd/api/build/
+            cp -v $cwd/build/{character-generator,name-generator,roll,openrpg} $cwd/api/build/ 
+
+            echo "FINISHED.... BUILDING API..."
+
+
+            pushd $cwd/api/
+
+            cargo build
+
+            popd
+        else
+            echo "CARGO.TOML NOT FOUND..."
+        fi
+    else
+        echo "CARGO/RUST NOT INSTALLED..."
+    fi
 fi
