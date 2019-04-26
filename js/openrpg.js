04877223efe883c9bbca993aaa7a2b7612787c16
incomingstick <incomingstick@gmail.com>
2019-04-26 00:09:34 -0500

    added cookie usage warning for Google Analytics

--- js/openrpg.js
+++ js/openrpg.js
@@ -1,1 +1,15 @@
 /* Custom JavaScript here */
+$(function () {
+    if (document.cookie.indexOf("cookies") < 0) {
+        $("#cookie-consent").slideDown('slow');
+    }
+
+    $("#consent").on('click', function() {
+        document.cookie = "cookies=yes; max-age=" + (365 * 52 * 7 * 24 * 60 * 60);
+        $("#cookie-consent").slideUp('slow');
+    });
+
+    $("#learn-more").on('click', function() {
+        location.href = '/about/privacy/';
+    });
+});
