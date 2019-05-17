94574e8571779657217a9809463e9f00bf8e3973
incomingstick <incomingstick@gmail.com>
2019-05-17 14:08:50 -0500

    updated build.bat to more closely follow build.sh
    the install flag does not actually install yet, currently it is no
    different than the release flag

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ buildVars=''
 # TODO print a help output for this script.
 # This should not have to adhear to our help output standards
 # as the only people really ever running this script are devs
-if [[ $1 == "help" || $1 == "h" ]]; then
+if [[ $1 == "help" || $1 == "-h" ]]; then
     echo "This option is not yet supported: $1"
     exit 1
 fi
