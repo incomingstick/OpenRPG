73a0389b3c3a346b527e784df7bb5dcc393902db
incomingstick <incomingstick@gmail.com>
2019-05-17 14:40:58 -0500

    fixed a bug in the help command that would cause an EXIT_FAILURE

--- src/openrpg.cpp
+++ src/openrpg.cpp
@@ -77,15 +77,18 @@ static void print_basic_help() {
     fputs("openrpg " VERSION " - " COPYRIGHT "\n"
           "OpenRPG Software License - Version 1.0 - February 10th, 2017 < https://www.openrpg.io/about/license/ >\n"
           "This is free software: you are free to change and redistribute it.\n"
-          "There is NO WARRANTY, to the extent permitted by law.\n\n"
+          "There is NO WARRANTY, to the extent permitted by law.\n"
+          "\n"
           "Usage: orpg > [command]\n"
           "\n"
           "Available commands:\n"
-                "\thelp (h) [COMMAND]                  Print the help screen for the given module. Omitting a\n"
-                "\t                                      command prints this help menu.\n" 
-                "\tgenerate (gen, ng) [RACE | GENDER]  Generate a random name of the given RACE and GENDER\n"
-                "\troll (r) [XdY]                      Simulates rolling dice with Y sides X number of times\n"
-                "\tversion (ver, v | V)                Print version info\n"
+                "\thelp (h) [module]                    Print the help screen for the given module. Omitting a\n"
+                "\t                                         module prints this help menu.\n" 
+                "\tversion (ver, v | V)                 Print version info\n"
+          "\n"
+          "Available modules:\n"
+                "\tgenerate (gen, ng) [RACE | GENDER]   Generate a random name of the given RACE and GENDER\n"
+                "\troll (r) [XdY]                       Simulates rolling dice with Y sides X number of times\n"
           "\n"
           "Long options may not be passed with a single dash.\n"
           "OpenRPG home page: < https://www.openrpg.io >\n"
@@ -285,6 +288,10 @@ int parse_input(string in) {
                         Names::print_basic_help();
 
                         return CONTINUE_CODE;
+                    } else {
+                        print_basic_help();
+
+                        return CONTINUE_CODE;
                     }
                 } else {
                     print_basic_help();
@@ -329,8 +336,8 @@ int main(int argc, char* argv[]) {
     int status = parse_args(argc, argv); // may exit
     
     if(status == CONTINUE_CODE) {
-        // TODO - clgui for program
         if(!QUIET_FLAG)
+            // TODO - add more banners and randomly pick one
             print_file("banners/welcome_mat1");
         string in("");
 
@@ -339,7 +346,8 @@ int main(int argc, char* argv[]) {
 #           ifndef _WIN32
                 printf("\33[4morpg\33[0m > ");
 #           else
-                printf("orpg > "); // Windows does not currently support linux style terminal text editing
+                // Windows does not currently support linux style terminal text editing
+                printf("orpg > ");
 #           endif
             
             getline(cin, in);
