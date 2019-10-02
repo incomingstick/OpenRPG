a93575a8c3b1940df730ed716cfa03d1af389ffd
incomingstick <incomingstick@gmail.com>
2019-10-01 21:53:15 -0500

    fixed export.js to account for differing OS build locations and version bump

--- wrappers/export.js
+++ wrappers/export.js
@@ -1,3 +1,23 @@
-const libroll = require('../build/Release/rollAddon');
+var os = require('os');
+
+/**
+ * define the location to import the node addon wrappers from
+ * Windows:         ../build/Releasse/
+ * Linux and macOS: ../build/lib/openrpg/
+ **/
+let prefix;
+
+if (os.type() === 'Linux' || os.type() === 'Darwin') {
+    prefix = '../build/lib/openrpg/';
+}
+else if (os.type() === 'Windows_NT') {
+    prefix = '../build/Release/';
+}
+else
+   throw new Error("Unsupported OS found: " + os.type());
+
+
+// define the export libraries here
+const libroll = require(prefix + 'rollAddon');
 
 module.exports = libroll;
